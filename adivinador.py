43b0b02da1c41cabd2629360cb604b71d270283b
Joaquin Rodil <joaquinpatricio2013@gmail.com>
2020-10-13 17:57:12 -0300

    segundo commit

---
+++ adivinador.py
@@ -0,0 +1,19 @@
+import random
+print("Pensa un numero entre 1 y 20")
+print("")
+temp = input("Tu numero es mayor que 10? (Y/N)")
+if temp == "Y":
+	temp = input("Tu numero es mayor que 15? (Y/N)")
+	if temp == ("Y"):
+		print("Tu numero es: "+str(random.randint(15,20)))
+	else:
+		print("Tu numero es: "+str(random.randint(10,15)))
+else:
+	temp = input("Tu numero es mayor que 5? (Y/N)")
+	if temp == ("Y"):
+		print("Tu numero es: "+str(random.randint(5,10)))
+	else:
+		print("Tu numero es: "+str(random.randint(1,5)))
+print("(La empresa no se hace cargo si no sale tu numero)")
+
+
