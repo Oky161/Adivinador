793410ecff65cb66a666ec869aa1b5051a7dcb0d
LucasxSilva <71045420+LucasxSilva@users.noreply.github.com>
2020-10-13 18:25:18 -0300

    Update adivinador.py

--- adivinador.py
+++ adivinador.py
@@ -1,19 +1,19 @@
 import random
-print("Pensa un numero entre 1 y 20")
+print("Pensa un numero entre 1 y 80")
 print("")
-temp = input("Tu numero es mayor que 10? (Y/N)")
+temp = input("Tu numero es mayor que 40? (Y/N)")
 if temp == "Y":
-	temp = input("Tu numero es mayor que 15? (Y/N)")
+	temp = input("Tu numero es mayor que 60? (Y/N)")
 	if temp == ("Y"):
-		print("Tu numero es: "+str(random.randint(15,20)))
+		print("Tu numero es: "+str(random.randint(60,80)))
 	else:
-		print("Tu numero es: "+str(random.randint(10,15)))
+		print("Tu numero es: "+str(random.randint(40,60)))
 else:
-	temp = input("Tu numero es mayor que 5? (Y/N)")
+	temp = input("Tu numero es mayor que 20? (Y/N)")
 	if temp == ("Y"):
-		print("Tu numero es: "+str(random.randint(5,10)))
+		print("Tu numero es: "+str(random.randint(20,40)))
 	else:
-		print("Tu numero es: "+str(random.randint(1,5)))
+		print("Tu numero es: "+str(random.randint(1,20)))
 print("(La empresa no se hace cargo si no sale tu numero)")
 
 
